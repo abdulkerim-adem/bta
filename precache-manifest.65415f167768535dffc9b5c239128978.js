881f29bbd6bf179ad7421d879447b70cc0d8ed40
abdulkerim-adem <abdulkerimadem36@gmail.com>
2022-11-16 10:52:55 +0300

    Updates

---
+++ precache-manifest.65415f167768535dffc9b5c239128978.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29ca970bc745efbc6386",
+    "url": "/bts/static/css/main.7ea17198.chunk.css"
+  },
+  {
+    "revision": "29ca970bc745efbc6386",
+    "url": "/bts/static/js/main.29ca970b.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/bts/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/bts/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "5159e19c1fd48ad19c80",
+    "url": "/bts/static/js/runtime~main.5159e19c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/bts/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e5570bd48fe04e53a17494cd0afad0b6",
+    "url": "/bts/index.html"
+  }
+];
